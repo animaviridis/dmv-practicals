cf67136187fb5fcdbed5c2467be04377c3c6bea3
mysh <mysh995@gmail.com>
2020-04-14 23:04:20 +0100

    [Assignment 1][Task 1] Kmeans - Euclidean pairs

--- assessment/assignment1/task1/kmeans.py
+++ assessment/assignment1/task1/kmeans.py
@@ -38,8 +38,8 @@ class KMeans(object):
         self._assign_points()
 
     @staticmethod
-    def _compute_euclidean_distances(arr1, arr2):
-        """Compute Euclidean distances between two arrays.
+    def _euclidean_matrix(arr1, arr2):
+        """Compute matrix of Euclidean distances between two arrays.
 
         Parameters
         ----------
@@ -54,14 +54,28 @@ class KMeans(object):
         def expand(a1, a2, axis=0):
             return np.stack(a2.shape[0]*(a1,), axis=axis)
 
-        diff = expand(arr1, arr2, 0) - expand(arr2, arr1, 1)
+        return KMeans._euclidean_pairs(expand(arr1, arr2, 0), expand(arr2, arr1, 1))
 
-        return (diff**2).sum(axis=-1)**0.5
+    @staticmethod
+    def _euclidean_pairs(arr1, arr2):
+        """Compute Euclidean pairwise distances between two arrays of the same shape (first-first, second-second etc.)
+
+        Parameters
+        ----------
+        arr1, arr2  :   numpy.ndarray
+            two arrays to compute the distances for; shape: (n_samples, n_dimensions) - same for both arrays
+
+
+        Returns
+        -------
+        a np.ndarray of Euclidean distances of the same shape as the input arrays."""
+
+        return ((arr1 - arr2)**2).sum(axis=-1)**0.5
 
     def _assign_points(self):
         """Assign data points to clusters. Return an array of cluster labels."""
 
-        self._labels = self._compute_euclidean_distances(self._data, self._centroids).argmin(axis=0)
+        self._labels = self._euclidean_matrix(self._data, self._centroids).argmin(axis=0)
 
 
 
