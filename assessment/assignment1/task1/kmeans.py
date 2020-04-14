703f87dab2681d8771b0a8a0d2a57b281df2fd77
mysh <mysh995@gmail.com>
2020-04-14 17:15:21 +0100

    [Assignment 1][Task 1] Kmeans - picking initial centroids

--- assessment/assignment1/task1/kmeans.py
+++ assessment/assignment1/task1/kmeans.py
@@ -11,12 +11,27 @@ class KMeans(object):
         self._k = k
 
         self._data = None
+        self._labels = None
+        self._centroids = None
 
     @property
     def k(self):
         return self._k
 
+    @property
+    def labels(self):
+        return self._labels
+
+    @property
+    def centroids(self):
+        return self._centroids
+
+    def _pick_centroids(self, n_centroids=None):
+        n_centroids = n_centroids or self._k
+        return self._data[np.random.choice(np.arange(self._data.shape[0]), n_centroids)]
+
     @misc.check_types
     def fit(self, data: np.ndarray):
         self._data = data
+        self._centroids = self._pick_centroids()
 
